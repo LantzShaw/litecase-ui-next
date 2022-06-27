042670750e915f7ad634971899e588ec576e8b37
Lantz <lantzshaw@163.com>
2022-06-27 16:31:47 +0800

    feat: add button and input component

---
+++ packages/components/Button/Button.tsx
@@ -0,0 +1,22 @@
+import React, { FC, ReactElement, MouseEventHandler, ReactNode } from 'react';
+
+export interface BaseButtonProps {
+  type?: 'primary' | 'secondary' | 'default';
+  children?: ReactNode;
+}
+
+export type PrimaryButtonProps = {
+  onClick?: () => MouseEventHandler<HTMLButtonElement>;
+} & BaseButtonProps;
+
+const Button: FC<PrimaryButtonProps> = (props): ReactElement => {
+  const { type = 'default', ...rest } = props;
+
+  return (
+    <>
+      <button>{props.children}</button>
+    </>
+  );
+};
+
+export default Button;
